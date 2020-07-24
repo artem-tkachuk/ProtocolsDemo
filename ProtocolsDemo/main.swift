6aa2229a0777f64d8c844fa57ed2e1fd2366eb0b
Artem <Tkachuk>
2020-07-24 14:58:40 -0700

    Done

--- ProtocolsDemo/main.swift
+++ ProtocolsDemo/main.swift
@@ -6,7 +6,62 @@
 //  Copyright © 2020 Artem Tkachuk. All rights reserved.
 //
 
-import Foundation
+protocol CanFly {
+    func fly()
+}
 
-print("Hello, World!")
+class Bird {
+    var isFemaile = true
+    
+    func layEgg() {
+        if isFemaile {
+            print("The bird makes a new bird in a shell.")
+        }
+    }
+}
+
+class Eagle: Bird, CanFly {
+    func fly() {
+        print("The eagle flaps its wings and lifts off into the sky.")
+    }
+    
+    func soar() {
+        print("The eagle glides in the air using air currents.")
+    }
+}
+
+class Penguin: Bird {
+    func swim() {
+        print("Penguin paddles through the water")
+    }
+}
+
+struct FlyingMuseum {
+    func flyingDemo(flyingObject: CanFly) {
+        flyingObject.fly()
+    }
+}
+
+struct Airplane: CanFly {
+    func fly() {
+        print("The airplane uses its engine to life off into the air.")
+    }
+}
+
+let myEagle = Eagle()
+myEagle.fly()
+myEagle.layEgg()
+myEagle.soar()
+
+let myPenguin = Penguin()
+myPenguin.layEgg()
+myPenguin.swim()
+//myPenguin.fly() // Our penguin inherits some functionality that it should not
+
+let museum = FlyingMuseum()
+museum.flyingDemo(flyingObject: myEagle)
+//museum.flyingDemo(flyingObject: myPenguin)
+
+let myPlane = Airplane()
+museum.flyingDemo(flyingObject: myPlane)
 
