4dd2a45fd9648a045354f2944c67845c254e6b8d
Artem <Tkachuk>
2020-07-24 14:06:09 -0700

    Initial Commit

---
+++ ProtocolsDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProtocolsDemo
+//
+//  Created by Artem Tkachuk on 7/24/20.
+//  Copyright © 2020 Artem Tkachuk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
